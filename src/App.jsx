329b6b8c7c0dba79a21de8617a32480ce6836863
Tendry <tendryzephyrin@gmail.com>
2024-08-15 05:57:40 +0200

    Finition du Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Layout } from "./components/Layout/Layout";
 import { About } from "./components/about/About";
 import { Competence } from "./components/competence/Competence";
+import Footer from "./components/footer/footer";
 import { Home } from "./components/home/Home";
 import { NavBar } from "./components/navbar/NavBar";
 import { Project } from "./components/project/project";
@@ -21,6 +22,7 @@ function App () {
                 <Service/>
                 <Competence/>
                 <Project/>
+                <Footer/>
             </Layout>
         </ThemeProvider>
     )
@@ -28,89 +30,3 @@ function App () {
 }
 
 export default App
-
-// import React, { useState, useEffect, useRef } from 'react';
-// import fond from './assets/images/dark-magical-forest-desktop-wallpaper.jpg'
-
-// const Carousel = ({ slides }) => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-//   const [visibleSlides, setVisibleSlides] = useState(1);
-//   const carouselRef = useRef();
-
-//   const getVisibleSlides = () => {
-//     const containerWidth = carouselRef.current.clientWidth;
-//     const slideWidth = carouselRef.current.querySelector('.slide').clientWidth;
-//     return Math.round(containerWidth / slideWidth);
-//   };
-
-//   const updateVisibleSlides = () => {
-//     setVisibleSlides(getVisibleSlides());
-//   };
-
-//   useEffect(() => {
-//     updateVisibleSlides();
-//     window.addEventListener('resize', updateVisibleSlides);
-//     return () => window.removeEventListener('resize', updateVisibleSlides);
-//   }, []);
-
-//   const showSlide = (index) => {
-//     const width = carouselRef.current.querySelector('.slide').clientWidth;
-//     carouselRef.current.style.transform = `translateX(${-index * width}px)`;
-//   };
-
-//   const nextSlide = () => {
-//     const maxIndex = slides.length - visibleSlides;
-//     const newIndex = Math.min(currentIndex + visibleSlides, maxIndex);
-//     setCurrentIndex(newIndex);
-//     showSlide(newIndex);
-//   };
-
-//   const prevSlide = () => {
-//     const newIndex = Math.max(currentIndex - visibleSlides, 0);
-//     setCurrentIndex(newIndex);
-//     showSlide(newIndex);
-//   };
-
-//   useEffect(() => {
-//     showSlide(currentIndex);
-//   }, [currentIndex, visibleSlides]);
-
-//   return (
-//     <div className="carousel-container">
-//       <div className="carousel" ref={carouselRef}>
-//         {slides.map((slide, index) => (
-//           <div key={index} className="slide">
-//             {slide}
-//           </div>
-//         ))}
-//       </div>
-//       <button className="prev-btn" onClick={prevSlide} style={{ display: currentIndex === 0 ? 'none' : 'block' }}>
-//         &#10094;
-//       </button>
-//       <button className="next-btn" onClick={nextSlide} style={{ display: currentIndex >= slides.length - visibleSlides ? 'none' : 'block' }}>
-//         &#10095;
-//       </button>
-//     </div>
-//   );
-// };
-
-// // src/App.js
-// const slides = [
-//   <div className="image"><img src={fond} alt="Gestion de pharmacie" /></div>,
-//   <div className="image"><img src={fond} alt="Gestion d'élection" /></div>,
-//   <div className="image"><img src={fond} alt="Site e-commerce" /></div>,
-//   <div className="image"><img src={fond} alt="UpWork" /></div>,
-//   <div className="image"><img src={fond} alt="QUIZZ en JavaScript" /></div>,
-// ];
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Carousel slides={slides} />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,21 @@
+// Footer.js
+import React from 'react';
+import '../../assets/style/footer.css';
+
+const Footer = () => {
+     return (
+          <footer className="footer">
+               <div className="footer-content">
+                    <p>&copy; 2024 Mon Portfolio. Tous droits réservés.</p>
+                    <div className="social-icons">
+                         <a href="mailto:tendryzephyrin@gmail.com"><i className='bx bx-envelope'></i></a>
+                         <a href="https://github.com/Tendry-Rkt56"><i className='bx bxl-github'></i></a>
+                         <a href="https://web.facebook.com/rayan.andria.92"><i className='bx bxl-facebook'></i></a>
+                         <a href=""><i className='bx bxl-twitter'></i></a>
+                    </div>
+               </div>
+          </footer>
+     )
+}
+
+export default Footer;
